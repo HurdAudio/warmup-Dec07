86ab5b321aebc5668a80bc1b95e9f85bd9049757
Devin Hurd <hurdaudio@gmail.com>
2016-12-07 11:25:45 -0700

    updated

--- warmup.js
+++ warmup.js
@@ -9,51 +9,91 @@ var Cup = function (name, coldTempRating, hotTempRating, liquidIntegrity) {
   this.liquidIntegrity = liquidIntegrity;
   this.fillCup = false;
 
-  return {
-    name: this.name,
-    coldTempRating: this.coldTempRating,
-    hotTempRating: this.hotTempRating,
-    fillCup: this.fillCup,
-    hotBeverageFill: function(temp) {
-      if (this.fillcup) {
-        console.log('YOUR CUP RUNNETH OVER!');
-      }
-      if (temp > this.hotTempRating) {
-        this.liquidIntegrity = false;
-      }
-      if (!this.liquidIntegrity) {
-        console.log('Beverage is too hot, your cup has melted.');
-      } else {
-        this.fillCup = true;
-        console.log('Enjoy your tasty, hot beverage.');
-      }
-    },
+//   return {
+//     name: this.name,
+//     coldTempRating: this.coldTempRating,
+//     hotTempRating: this.hotTempRating,
+//     fillCup: this.fillCup,
+//     hotBeverageFill: function(temp) {
+//       if (this.fillcup) {
+//         console.log('YOUR CUP RUNNETH OVER!');
+//       }
+//       if (temp > this.hotTempRating) {
+//         this.liquidIntegrity = false;
+//       }
+//       if (!this.liquidIntegrity) {
+//         console.log('Beverage is too hot, your cup has melted.');
+//       } else {
+//         this.fillCup = true;
+//         console.log('Enjoy your tasty, hot beverage.');
+//       }
+//     },
+//
+//     coldBeverageFill: function(temp) {
+//       if (this.fillcup) {
+//         console.log('YOUR CUP RUNNETH OVER!');
+//       }
+//       if (temp < this.coldTempRating) {
+//         this.liquidIntegrity = false;
+//       }
+//       if (!this.liquidIntegrity) {
+//         console.log('Beverage is too cold, your cup has shattered.');
+//       } else {
+//         this.fillCup = true;
+//         console.log('Enjoy your cool beverage.');
+//       }
+//     },
+//
+//     drinkBeverage: function () {
+//       if (this.fillCup) {
+//         console.log('Mmmmm.... deslish.');
+//         this.fillCup = false;
+//       } else {
+//         console.log('Sadly, your cup is empty.');
+//       }
+//     }
+//
+//   };
+// };
 
-    coldBeverageFill: function(temp) {
-      if (this.fillcup) {
-        console.log('YOUR CUP RUNNETH OVER!');
-      }
-      if (temp < this.coldTempRating) {
-        this.liquidIntegrity = false;
-      }
-      if (!this.liquidIntegrity) {
-        console.log('Beverage is too cold, your cup has shattered.');
-      } else {
-        this.fillCup = true;
-        console.log('Enjoy your cool beverage.');
-      }
-    },
-
-    drinkBeverage: function () {
-      if (this.fillCup) {
-        console.log('Mmmmm.... deslish.');
-        this.fillCup = false;
-      } else {
-        console.log('Sadly, your cup is empty.');
-      }
+Cup.prototype.hotBeverageFill = function (temp) {
+  if (this.fillcup) {
+      console.log('YOUR CUP RUNNETH OVER!');
     }
+    if (temp > this.hotTempRating) {
+      this.liquidIntegrity = false;
+    }
+    if (!this.liquidIntegrity) {
+      console.log('Beverage is too hot, your cup has melted.');
+    } else {
+      this.fillCup = true;
+      console.log('Enjoy your tasty, hot beverage.');
+    }
+};
 
-  };
+Cup.prototype.coldBeverageFill = function (temp) {
+  if (this.fillcup) {
+     console.log('YOUR CUP RUNNETH OVER!');
+    }
+    if (temp < this.coldTempRating) {
+      this.liquidIntegrity = false;
+    }
+    if (!this.liquidIntegrity) {
+      console.log('Beverage is too cold, your cup has shattered.');
+    } else {
+      this.fillCup = true;
+      console.log('Enjoy your cool beverage.');
+   }
+};
+
+Cup.prototype.drinkBeverage = function (){
+  if (this.fillCup) {
+    console.log('Mmmmm.... deslish.');
+    this.fillCup = false;
+  } else {
+    console.log('Sadly, your cup is empty.');
+  }
+
 };
 
 var cupA = new Cup ("Cuppa Cuppa", -26, 184, true);
